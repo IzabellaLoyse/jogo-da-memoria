c5d6dd6ffa865df227661ca8796f51b527437694
Izabella Loyse <izabellaloyse13@gmail.com>
2021-07-07 20:15:14 -0300

    :tada: feat(projeto): Inicialização dos arquivos e pastas do projeto

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/generic.css";
+import "./src/styles/tools/NessBorder/styles.css";
+import "./src/styles/elements/base.css";
+import CardGame from "./src/components/CardGame";
+
+
+const $root = document.querySelector("#root");
+$root.insertAdjacentHTML("beforeend", CardGame());
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function CardGame() {
+	return /* html */ `
+  <figure class="c-card-game">
+  <img src="images/logo-pokebola.png" alt="Imagem de uma pokébola" 
+  class="c-icon" />
+  </figure>
+  
+  `;
+}
+
+export default CardGame;
