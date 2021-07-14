c5538b800e842373f79ba9e2c0207d93c2f08db7
Izabella Loyse <izabellaloyse13@gmail.com>
2021-07-14 20:26:26 -0300

    :sparkles: feat(CardFrontBack): Adicionado a Imagem da Pokebola e Pikachu

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,14 +1,13 @@
-import CardGame from "../CardGame";
+import CardGame from '../CardGame'
 
 function CardFrontBack() {
   return /* html */ `
     <article class="c-card-front-back">
     ${CardGame()}
-    ${CardGame()}
+    ${CardGame('pikachu', 'Imagem do Pikachu')}
     </article>
   
-  `;
+  `
 }
 
-
-export default CardFrontBack;
+export default CardFrontBack
