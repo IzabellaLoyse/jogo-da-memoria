681f1c844dca5103d4f4c73254517a08f56a543b
Izabella Loyse <izabellaloyse13@gmail.com>
2021-07-20 21:54:44 -0300

    :sparkles: Adicionado o Flip do CardGame

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,17 @@
-import CardGame from '../CardGame'
+import CardGame from '../CardGame';
+import './style.css';
+
 
 function CardFrontBack() {
   return /* html */ `
     <article class="c-card-front-back">
-    ${CardGame()}
-    ${CardGame('pikachu', 'Imagem do Pikachu')}
+    <div class="card -front">
+      ${CardGame()}
+    </div>
+
+    <div class="card -back">
+      ${CardGame('pikachu', 'Imagem do Pikachu')}
+    </div>
     </article>
   
   `
